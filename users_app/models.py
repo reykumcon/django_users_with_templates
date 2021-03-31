e7466409c057f2449f8b76e0f85c37ebfc3e6e79
Reymerson Concepcion <reymerson.concepcion@gmail.com>
2021-03-31 13:20:14 -1000

    Users With Templates Assignment

---
+++ users_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email_address = models.CharField(max_length=255)
+    age = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __repr__(self):
+        return f"User:\n First Name: {self.first_name}\n Last Name: {self.last_name}\n Email: {self.email_address}\n Age: {self.age}"
